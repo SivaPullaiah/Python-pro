1b478e56802896f5ab02da69794e666e434800ab
SivaPullaiah <mangalisivapullaiah@gmail.com>
2023-01-14 11:04:23 +0530

    Documentation of Procedure for pattern solving

---
+++ patterns solve/patternsolve.py
@@ -0,0 +1,6 @@
+#1. Observe the pattern for simple input.
+#2. Then draw the pattern for that simple input.
+#3. Make a note of each row. What are you doing?
+	Which means how many stars or numbers are printing, or the row numbers printing or column numbers printing?
+#4. Do! Third step for each Row.
+#5. Then you can easily understand and write the code also.
