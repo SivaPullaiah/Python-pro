5a5c9c21598f3d9084021559488d0ae474a0abb3
sivapullaiahmangali <122810340+sivapullaiahmangali@users.noreply.github.com>
2023-01-18 05:42:53 +0530

    points added

--- patterns solve/patternsolve.py
+++ patterns solve/patternsolve.py
@@ -1,6 +1,10 @@
 #1. Observe the pattern for simple input.
 #2. Then draw the pattern for that simple input.
 #3. Make a note of each row. What are you doing?
-	Which means how many stars or numbers are printing, or the row numbers printing or column numbers printing?
+# 	Which means how many stars or numbers are printing, or the row numbers printing or column numbers printing?
 #4. Do! Third step for each Row.
 #5. Then you can easily understand and write the code also.
+
+
+# Easy way is understand python patterns is divide in to rows and columns
+# and try to find relation of each row
